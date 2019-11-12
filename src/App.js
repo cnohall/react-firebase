c48bf95214c934c875be652e4738db7d728d2090
Christopher <christopher.nohall@gmail.com>
2019-11-12 21:57:35 +0800

    MVP reached

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import './App.css';
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBMvN9sjscQI8sg6E3ClJiWHFFzWU4poGE",
+  authDomain: "gustavhemsida.firebaseapp.com"
+})
+
+class App extends Component {
+  state = {isSignedIn : false}
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions : [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSucces : () => false
+    }
+  }
+
+componentDidMount = () => {
+
+
+  firebase.auth().onAuthStateChanged(user => {
+    this.setState({isSignedIn:!!user})
+  })
+}
+
+  render () {
+    return (
+    <div className="App">
+      {this.state.isSignedIn ? (
+        <div>Signed In!</div>
+      ) : (
+        <StyledFirebaseAuth
+          uiConfig={this.uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
+      )}
+  </div>
+    );
+  }
+}
+
+export default App;
